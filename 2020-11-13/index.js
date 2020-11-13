e6a0c320ba88702f41306949f6b0e568f5f68932
Min woo <dxki104@naver.com>
2020-11-13 23:43:13 +0900

    calulator

--- 2020-11-13/index.js
+++ 2020-11-13/index.js
@@ -39,13 +39,13 @@ console.log(multyple(4,9));
 
 
 function divide(num3,num4){
-    Number();
-    console.log(typeof num4);
-    return (num3/num4);
+    Number(); //넘버타입으로 바꾸는 함수
+    console.log(typeof num4); // 타입 확인하는 키워드
+    return (num3/num4); // 결과값 리턴
 }
 
-const divide1 = divide(123,123);
-const abc = 1;
+const divide1 = divide(123,123); 
+
 console.log(divide1);
 
 
