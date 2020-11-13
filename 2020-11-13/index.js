34c76dc3d7ec2482a5081cc0cf24645ed1f91d9f
Min woo <dxki104@naver.com>
2020-11-13 23:33:29 +0900

    function

---
+++ 2020-11-13/index.js
@@ -0,0 +1,64 @@
+//사칙연산 펑션 만들기
+
+
+
+//덧셈
+function sum(a,b){
+    return (a+b);
+}
+let sum1 = sum(2,3);
+console.log(sum1);
+
+console.log(sum(6,3));
+
+
+//뺼셈
+function minus(num1,num2){
+    return num1-num2;
+}
+minus(5,4);
+console.log(minus(5,4));
+
+
+
+
+
+
+//곱하기
+function multyple(A,B){
+    return A*B;
+}
+console.log(multyple(4,9));
+
+
+
+
+
+
+//나누기
+
+
+function divide(num3,num4){
+    Number();
+    console.log(typeof num4);
+    return (num3/num4);
+}
+
+const divide1 = divide(123,123);
+const abc = 1;
+console.log(divide1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
